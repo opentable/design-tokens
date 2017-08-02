5d97c2f724c1bb97aab628292390cf0c1990a07a
Nick Balestra <nbalestra@opentable.com>
2017-08-02 16:29:17 +0100

    updated build scripts

--- tools/ot-design-token-scripts/scripts/build.js
+++ tools/ot-design-token-scripts/scripts/build.js
@@ -3,8 +3,13 @@ const fs = require('fs-extra')
 const path = require('path')
 
 const formatSpecs = require('./utils/format-specs')
-
 const formats = process.argv.slice(2);
+
+theo.registerFormat('cssmodules.css', fs.readFileSync(path.join(__dirname, './utils/formats/cssmodules.css.hbs'), 'utf8'));
+
+const packagePath = process.cwd();
+fs.emptyDirSync(path.join(packagePath,'dist'));
+
 formats.forEach(format => {
   if (formatSpecs[format]) {
     const spec = formatSpecs[format]
@@ -18,7 +23,6 @@ formats.forEach(format => {
       }
     })
     .then(data => {
-      const packagePath = process.cwd();
       fs.outputFile(path.join(packagePath, `dist/index.${spec.extension}`), data)
         .then(()=>console.log(`😎  ${spec.type} token succesfully written at "${path.join(packagePath, `dist/index.${spec.extension}`)}"`))
     })
