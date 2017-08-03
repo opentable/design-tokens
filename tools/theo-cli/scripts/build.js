8e3c920d52152609fb611b087b4b4e1273c125f3
Nick Balestra <nbalestra@opentable.com>
2017-08-03 19:04:33 +0100

    clear distPath if not root

--- tools/theo-cli/scripts/build.js
+++ tools/theo-cli/scripts/build.js
@@ -11,33 +11,28 @@ const distPath = argv.dist || argv.d || '.';
 const fileName = argv.output || argv.o || 'token';
 const source = argv.src || argv.s || 'token.yml';
 
-fs.emptyDir(path.join(packagePath, 'dist'))
-.then(() => {
-  console.log();
-  formats.forEach(format => {
-    const outputFile = path.join(distPath, `${fileName}.${format}`);
-    theo.convert({
-      transform: {
-        type: 'raw',
-        file: path.join(packagePath, source)
-      },
-      format: {
-        type: format
-      }
-    })
-    .then(data => {
-      fs.outputFile(path.join(packagePath, outputFile), data)
-        .then(()=>console.log(`✏️  ${format} tokens created at "${path.basename(packagePath)}/${outputFile}"`))
-    })
-    .catch(error => {
-      console.log(`💩  Oups, something went wrong: ${error}`)
-      process.exit(1);
-    })
+if (distPath !== '.') {
+  fs.emptyDirSync(path.join(packagePath, distPath))
+}
+
+console.log();
+formats.forEach(format => {
+  const outputFile = path.join(distPath, `${fileName}.${format}`);
+  theo.convert({
+    transform: {
+      type: 'raw',
+      file: path.join(packagePath, source)
+    },
+    format: {
+      type: format
+    }
+  })
+  .then(data => {
+    fs.outputFile(path.join(packagePath, outputFile), data)
+      .then(()=>console.log(`✏️  ${format} tokens created at "${path.basename(packagePath)}/${outputFile}"`))
+  })
+  .catch(error => {
+    console.log(`💩  Oups, something went wrong: ${error}`)
+    process.exit(1);
   })
 })
-.catch(error => {
-  console.log(`💩  Oups, something went wrong: ${error}`)
-  process.exit(1);
-})
-
-
