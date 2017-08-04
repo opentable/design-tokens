9ac0a6b759c479aea3e42678ff7ced7ef0cc669d
Nick Balestra <nbalestra@opentable.com>
2017-08-04 13:58:20 +0100

    otkit tokens builds

---
+++ OTKit/otkit-spacing/token.common.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    
+    spacingXsmall: "0.25rem",
+    
+    spacingSmall: "0.5rem",
+    
+    spacingMedium: "1rem",
+    
+    spacingLarge: "2rem",
+    
+    spacingXlarge: "4rem",
+};
