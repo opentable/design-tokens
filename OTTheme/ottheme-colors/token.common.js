f853d3bc23071d69eb67f3b5fb2d225ab175783e
Nick Balestra <nbalestra@opentable.com>
2017-08-04 13:58:32 +0100

    ottheme tokens builds

---
+++ OTTheme/ottheme-colors/token.common.js
@@ -0,0 +1,107 @@
+
+module.exports = {
+    
+    colorSecondaryLighter: "rgba(35,123,152,0.14)",
+    
+    colorMustard: "#FCCF44",
+    
+    colorDark: "#333333",
+    
+    colorIcecream: "#72C9CA",
+    
+    opacityColor: "rgba(0,0,0,0.12)",
+    
+    colorBrandRed: "#DA3743",
+    
+    opacityColorDark: "rgba(0,0,0,0.2)",
+    
+    colorLightGray: "#ECECEC",
+    
+    colorSecondaryDarker: "#356273",
+    
+    starColor: "#FEAB6C",
+    
+    colorTransparent: "rgba(255,255,255,0.6)",
+    
+    colorMenu: "#1E1E1E",
+    
+    colorPremiumAccessBookModalBackground: "#A67AD3",
+    
+    colorJuneberry: "#356273",
+    
+    opacityColorLight: "rgba(0,0,0,0.06)",
+    
+    colorSecondary: "#237B98",
+    
+    colorBrandBlue: "#237B98",
+    
+    colorMacadamia: "#DBD9D5",
+    
+    colorLight: "#999999",
+    
+    colorDarker: "#000000",
+    
+    colorAlmond: "#C4BFB9",
+    
+    colorLightSilver: "#ACACAC",
+    
+    colorCashew: "#F3F2F1",
+    
+    opacityColorDarker: "rgba(0,0,0,0.5)",
+    
+    bodyBg: "#F7F7F7",
+    
+    colorDarkGray: "#878787",
+    
+    opacityColorDarkHex: "#CCCCCC",
+    
+    colorEarlyGirl: "#DA3743",
+    
+    colorBlackCherry: "#811520",
+    
+    colorCaspianPink: "#E5656E",
+    
+    colorBlueberry: "#60B2D0",
+    
+    colorMidGray: "#CCCCCC",
+    
+    colorAlert: "#D30303",
+    
+    colorChalkboard: "#333333",
+    
+    colorOffersPrimary: "#3B9568",
+    
+    colorSilver: "#666666",
+    
+    darkenPercent: "10%",
+    
+    colorPrimaryDarker: "#811520",
+    
+    colorPrimary: "#DA3743",
+    
+    opacityColorLightHex: "#F0F0F0",
+    
+    colorGranite: "#444444",
+    
+    contentBg: "#F7F7F7",
+    
+    colorGifts: "#AED6D7",
+    
+    colorSuccess: "#75B96F",
+    
+    colorLogoSecondary: "#FFFFFF",
+    
+    opacityColorWhite: "rgba(255,255,255,0.3)",
+    
+    colorPlum: "#AA6286",
+    
+    colorMenuLink: "rgba(96,178,208,0.14)",
+    
+    colorHuckleberry: "#B1D9E7",
+    
+    colorPumpkin: "#EF8F4D",
+    
+    colorMedium: "#666666",
+    
+    colorWhite: "#FFFFFF",
+};
