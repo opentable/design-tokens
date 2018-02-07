113f1ca79b9a0c67a78ce7c895181edf91c9cf45
Nick Balestra <nbalestra@opentable.com>
2018-02-07 14:40:47 +0000

    hot-reloading single develop cmd for build tokens & develop style-guide

--- style-guide/src/pages/otkit-desktop-typography.js
+++ style-guide/src/pages/otkit-desktop-typography.js
@@ -1,3 +1,8 @@
 import React from "react";
+import styles from "otkit-desktop-typography/token.common";
 
-export default () => <div>Hello world!</div>;
+export default () => (
+  <div>
+    Hello token! <pre>{JSON.stringify(styles, null, 2)}</pre>
+  </div>
+);
