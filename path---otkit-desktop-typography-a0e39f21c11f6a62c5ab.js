76c114004adff30d61900d5bcf6015413d6535c4
Nick Balestra <nbalestra@opentable.com>
2018-02-12 13:02:18 +0000

    Updates

--- path---otkit-desktop-typography-a0e39f21c11f6a62c5ab.js
+++ path---otkit-desktop-typography-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xd4d0fd0a639],{263:function(t,n){t.exports={pathContext:{}}}});
+webpackJsonp([0xd4d0fd0a639],{280:function(t,n){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---otkit-desktop-typography-a0e39f21c11f6a62c5ab.js.map
