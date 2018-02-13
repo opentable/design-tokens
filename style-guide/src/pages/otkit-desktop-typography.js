5d2d3c5eaab17533812c66061cf3bc16ff0692a1
Frances Yun <fyun668@gmail.com>
2018-02-12 16:40:29 -0800

    Make js and css prettier

--- style-guide/src/pages/otkit-desktop-typography.js
+++ style-guide/src/pages/otkit-desktop-typography.js
@@ -1,66 +1,71 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import _ from 'lodash';
+import React from "react";
+import Link from "gatsby-link";
+import _ from "lodash";
 
-import token from 'otkit-desktop-typography/token.common';
-import styles from '../styles/otkit-desktop-typography.module.css';
+import token from "otkit-desktop-typography/token.common";
+import styles from "../styles/otkit-desktop-typography.module.css";
 
-const typographyGroups = [{
-		name: "heading-xlarge",
-		isAlternate: false
-	}, 
+const typographyGroups = [
+  {
+    name: "heading-xlarge",
+    isAlternate: false
+  },
   {
     name: "heading-large",
     isAlternate: false
-  }, 
-	{
-		name: "heading-large",
-		isAlternate: true
-	}, 
-	{
-		name: "heading-medium",
-		isAlternate: false
-	}, 
-	{
-		name: "subheading-small",
-		isAlternate: false
-	}, 
-	{
-		name: "bodytext-small",
-		isAlternate: false
-	}, 
+  },
+  {
+    name: "heading-large",
+    isAlternate: true
+  },
+  {
+    name: "heading-medium",
+    isAlternate: false
+  },
+  {
+    name: "subheading-small",
+    isAlternate: false
+  },
+  {
+    name: "bodytext-small",
+    isAlternate: false
+  },
   {
     name: "subtext-xsmall",
     isAlternate: false
   },
-	{
-		name: "subtext-xsmall",
-		isAlternate: true
-	}
+  {
+    name: "subtext-xsmall",
+    isAlternate: true
+  }
 ];
 
 export default () => {
-  const groups = typographyGroups.map((group) => {
+  const groups = typographyGroups.map(group => {
     const fontSize = token[_.camelCase(`${group.name}-font-size`)];
-    const fontWeight = token[_.camelCase(`${group.name}-font-weight${group.isAlternate ? '-alternate' : ''}`)];
+    const fontWeight =
+      token[
+        _.camelCase(
+          `${group.name}-font-weight${group.isAlternate ? "-alternate" : ""}`
+        )
+      ];
     const lineHeight = token[_.camelCase(`${group.name}-line-height`)];
-    const groupName = `${group.name}${group.isAlternate ? '-alternate' : ''}`;
+    const groupName = `${group.name}${group.isAlternate ? "-alternate" : ""}`;
     const divStyle = {
       fontSize,
       fontWeight,
       lineHeight
     };
     return (
-      <div className={styles['font-item']} style={divStyle}>
-        {groupName} | font-size: {fontSize} | font-weight: {fontWeight} | line-height: {lineHeight}
+      <div className={styles["font-item"]} style={divStyle}>
+        {groupName} | font-size: {fontSize} | font-weight: {fontWeight} |
+        line-height: {lineHeight}
       </div>
     );
-  })
+  });
   return (
     <div>
-      <div className={styles['font-column']}>
-        {groups}
-      </div>
+      <div className={styles["font-column"]}>{groups}</div>
       <Link to="/">back to index</Link>
     </div>
   );
