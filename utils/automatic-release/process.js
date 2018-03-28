14462fdfda83aaa6433deb26c147cd882822f287
Nick Balestra <nick@balestra.ch>
2018-03-28 13:56:56 +0100

    wip

--- utils/automatic-release/process.js
+++ utils/automatic-release/process.js
@@ -18,7 +18,8 @@ const checkUPdated = () => {
     'node',
     ['node_modules/.bin/lerna', 'updated', '--json', '--loglevel=silen'],
     {
-      cwd: root
+      cwd: root,
+      stdout: 'inherit'
     }
   );
   let result = '';
@@ -40,7 +41,7 @@ const installLatestFromNPM = packages => {
   fs.ensureDirSync(latestTemp);
   fs.writeJsonSync(`${latestTemp}/package.json`, { name: 'release' });
 
-  const installed = packages.map(pkg => {
+  return packages.map(pkg => {
     const cmd = spawn('npm', ['install', pkg], {
       stdio: 'inherit',
       cwd: latestTemp
@@ -49,83 +50,102 @@ const installLatestFromNPM = packages => {
       cmd.on('close', code => (code === 0 ? resolve(pkg) : resolve(null)));
     });
   });
-
-  return installed;
 };
 
 const run = doPublish => {
-  return new Promise((resolve, reject) => {
-    checkUPdated()
-      .then(updated => {
-        Promise.all(installLatestFromNPM(updated)).then(() => {
-          updated.forEach(pkg => {
-            const updatedContent = yaml.load(
-              path.join(root, `${packagePathResolver(pkg)}/token.yml`)
-            );
-
-            // If no version has been published yet, publish as major
-            let version = 'major';
-            let diff;
-            const latestPath = path.join(
-              latestTemp,
-              'node_modules',
-              pkg,
-              'token.yml'
-            );
-
-            if (fs.pathExistsSync(latestPath)) {
-              const latestContent = yaml.load(latestPath);
-              diff = differ(latestContent.props, updatedContent.props);
-              version = diffToSemver(latestContent.props, updatedContent.props);
-            }
-
-            if (version !== undefined) {
-              // spawn lerna publish --force-publish=package --cd-version=version --yes
-              // https://github.com/lerna/lerna#--force-publish-packages
-              // https://github.com/lerna/lerna#--yes
-              // https://github.com/lerna/lerna#--cd-version
-              console.log(
-                `${
-                  doPublish ? 'publishing' : 'should publish'
-                } ${pkg} as ${version}`
+  return new Promise((resolvePublish, rejectPublish) => {
+    checkUPdated().then(updated => {
+      Promise.all(installLatestFromNPM(updated))
+        .then(installed =>
+          installed.map(pkg => {
+            return new Promise((resolve, reject) => {
+              const updatedContent = yaml.load(
+                path.join(root, `${packagePathResolver(pkg)}/token.yml`)
               );
-              if (doPublish) {
-                const cmd = spawn(
-                  'npm',
-                  [
-                    'run',
-                    'publish',
-                    '--',
-                    `--force-publish=${pkg}`,
-                    `--cd-version=${version}`,
-                    '--yes'
-                  ],
-                  {
-                    cwd: root
-                  }
+
+              // If no version has been published yet, publish as major
+              let version = 'major';
+              let diff;
+              const latestPath = path.join(
+                latestTemp,
+                'node_modules',
+                pkg,
+                'token.yml'
+              );
+              if (fs.pathExistsSync(latestPath)) {
+                const latestContent = yaml.load(latestPath);
+                diff = differ(latestContent.props, updatedContent.props);
+                version = diffToSemver(
+                  latestContent.props,
+                  updatedContent.props
                 );
-
-                cmd.stdout.on('data', data => console.log(data.toString()));
-
-                cmd.on('error', err => {
-                  console.log(`Error publishing ${pkg} as ${version}`, err);
-                  throw err;
-                });
               }
-            } else {
-              console.log(`Skipping publishing ${pkg}`);
-            }
-            console.log(diff);
-            console.log('--------------------------------');
-          });
+
+              if (version !== undefined) {
+                // spawn lerna publish --force-publish=package --cd-version=version --yes
+                // https://github.com/lerna/lerna#--force-publish-packages
+                // https://github.com/lerna/lerna#--yes
+                // https://github.com/lerna/lerna#--cd-version
+                console.log(
+                  `${
+                    doPublish ? 'publishing' : 'should publish'
+                  } ${pkg} as ${version}`
+                );
+                if (doPublish) {
+                  console.log(diff);
+                  console.log('--------------------------------');
+                  const cmd = spawn(
+                    'npm',
+                    [
+                      'run',
+                      'publish',
+                      '--',
+                      `--force-publish=${pkg}`,
+                      `--cd-version=${version}`,
+                      '--yes'
+                    ],
+                    {
+                      cwd: root,
+                      stdout: 'inherit'
+                    }
+                  );
+
+                  cmd.stdout.on('data', data => console.log(data.toString()));
+                  cmd.on('error', err => {
+                    console.log(`Error publishing ${pkg} as ${version}`, err);
+                    reject(err);
+                  });
+                  cmd.on(
+                    'close',
+                    code => (code === 0 ? resolve(pkg) : reject(code))
+                  );
+                } else {
+                  console.log(`Skipping publishing ${pkg}`);
+                  console.log(diff);
+                  console.log('--------------------------------');
+                  resolve(pkg);
+                }
+              }
+            });
+          })
+        )
+        .then(published => {
           fs.removeSync(latestTemp);
-          resolve(`Automatic release ${doPublish ? '' : 'test'} successful`);
+          resolvePublish(
+            `Automatic release ${doPublish ? '' : 'test'} successful`
+          );
+        })
+        .catch(err => {
+          rejectPublish(
+            `Automatic release ${
+              doPublish ? '' : 'test'
+            } failed with err ${err}`
+          );
         });
-      })
-      .catch(err => {
-        fs.removeSync(latestTemp);
-        reject(err);
-      });
+    });
+  }).catch(err => {
+    fs.removeSync(latestTemp);
+    reject(err);
   });
 };
 
