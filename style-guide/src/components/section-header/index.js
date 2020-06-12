a5d85a480376aea52cd89fd71cf8bfd27cbeea75
Ronnie Chong <ronniechong@users.noreply.github.com>
2020-06-12 14:00:40 +1000

    feat(styleguide): adds themeable icon styleguide (#491)
    * fix(docs): updates README
    
    * feat(styleguide): adds themeable icon styleguide

--- style-guide/src/components/section-header/index.js
+++ style-guide/src/components/section-header/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-export default function SectionHeader({ text, type }) {
+export default function SectionHeader({ text, type, content }) {
   return (
-    <h2
-      className={
-        type === 'SectionHeader__small'
-          ? styles['section-header-small']
-          : styles['section-header']
-      }
-    >
-      {text}
-    </h2>
+    <div className={styles['section-container']}>
+      <h2
+        className={
+          type === 'SectionHeader__small'
+            ? styles['section-header-small']
+            : styles['section-header']
+        }
+      >
+        {text}
+      </h2>
+      {content && <div className={styles['section-content']}>{content}</div>}
+    </div>
   );
 }
