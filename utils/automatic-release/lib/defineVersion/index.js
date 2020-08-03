a1c2fd9a19a0f3c5457582f0b18cb855b1b58456
Ronnie Chong <ronnie@otakuness.com>
2020-08-03 16:58:39 +1000

    fix(automatic-release): fixes encoding and uses fs

--- utils/automatic-release/lib/defineVersion/index.js
+++ utils/automatic-release/lib/defineVersion/index.js
@@ -3,7 +3,8 @@
 const yaml = require('js-yaml');
 const path = require('path');
 const packagePathResolver = require('./packagePathResolver');
-const fs = require('fs-extra');
+const fsExtra = require('fs-extra');
+const fs = require('fs');
 const differ = require('jest-diff');
 const diffToSemver = require('./diffToSemver');
 
@@ -11,15 +12,16 @@ const defineVersion = (pkg, root, latestTemp) => {
   let version, diff;
 
   const updatedContent = yaml.safeLoad(
-    fs.readFileSync(
-      path.join(root, `${packagePathResolver(pkg)}/token.yml`),
-      'utf-8'
-    )
+    fs.readFileSync(path.join(root, `${packagePathResolver(pkg)}/token.yml`), {
+      encoding: 'utf-8'
+    })
   );
   const latestPath = path.join(latestTemp, 'node_modules', pkg, 'token.yml');
 
-  if (fs.pathExistsSync(latestPath)) {
-    const latestContent = yaml.safeLoad(fs.readFileSync(latestPath), 'utf-8');
+  if (fsExtra.pathExistsSync(latestPath)) {
+    const latestContent = yaml.safeLoad(fs.readFileSync(latestPath), {
+      encoding: 'utf-8'
+    });
     diff = differ(latestContent.props, updatedContent.props);
     version = diffToSemver(latestContent.props, updatedContent.props);
   }
