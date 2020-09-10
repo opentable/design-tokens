d2687e94f885d16ae5a6ffb211498be48731b697
Ronnie Chong <ronnie@otakuness.com>
2020-09-10 13:19:49 +1000

    fix(style-guide): adds trailing slash

--- style-guide/src/components/design-nav/design-nav.js
+++ style-guide/src/components/design-nav/design-nav.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styles from '../../styles/index.module.scss';
 
+const isPartiallyActive = ({ isCurrent }) => {
+  return isCurrent ? { className: styles.linkActive } : null;
+};
 const NavLink = props => (
   <div className={styles.navLink}>
     <Link
       to={props.to}
-      partiallyActive={props.exact ? false : true}
+      getProps={isPartiallyActive}
       activeClassName={styles.linkActive}
     >
       {props.children}
@@ -17,17 +20,15 @@ const NavLink = props => (
 export default () => (
   <div className={styles.navSection}>
     <h4>DESIGN TOKENS</h4>
-    <NavLink exact to="/">
-      All Design Tokens
-    </NavLink>
-    <NavLink to="/otkit/colors">Colors</NavLink>
-    <NavLink to="/otkit/typography">Typography</NavLink>
-    <NavLink to="/otkit/grids">Grids</NavLink>
-    <NavLink to="/otkit/spacing">Spacing</NavLink>
-    <NavLink to="/otkit/borders">Borders</NavLink>
-    <NavLink to="/otkit/breakpoints">Breakpoints</NavLink>
-    <NavLink to="/otkit/shadows">Shadows</NavLink>
-    <NavLink to="/otkit/icons">Icons</NavLink>
-    <NavLink to="/otkit/icons-theme">Icons (theme)</NavLink>
+    <NavLink to="/">All Design Tokens</NavLink>
+    <NavLink to="/otkit/colors/">Colors</NavLink>
+    <NavLink to="/otkit/typography/">Typography</NavLink>
+    <NavLink to="/otkit/grids/">Grids</NavLink>
+    <NavLink to="/otkit/spacing/">Spacing</NavLink>
+    <NavLink to="/otkit/borders/">Borders</NavLink>
+    <NavLink to="/otkit/breakpoints/">Breakpoints</NavLink>
+    <NavLink to="/otkit/shadows/">Shadows</NavLink>
+    <NavLink to="/otkit/icons/">Icons</NavLink>
+    <NavLink to="/otkit/icons-theme/">Icons (theme)</NavLink>
   </div>
 );
