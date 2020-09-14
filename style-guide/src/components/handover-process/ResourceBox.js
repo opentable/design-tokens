2d2bf6f1a65df56d05750c52bc0af192092bdce5
nsingh <nsingh@opentable.com>
2020-09-14 11:05:33 +0530

    feat(style-guide): Resource box styling

--- style-guide/src/components/handover-process/ResourceBox.js
+++ style-guide/src/components/handover-process/ResourceBox.js
@@ -4,12 +4,12 @@ import copy from '../../../static/handover-process';
 import styles from '../../styles/handover.module.scss';
 
 export default ({ resource }) => (
-  <div className={styles.resource}>
+  <div className={styles.resource} onClick={() => { window.open(copy[`${resource}.link`], '_blank')}}>
     <div className={styles.resourceTitle}>
       <BullseyeIcon />
       <div>{copy[`${resource}.title`]}</div>
     </div>
     <div className={styles.resourceDesc}>{copy[`${resource}.description`]}</div>
-    <a className={styles.resourceLink} href={copy[`${resource}.link`]} target="_blank">{copy[`${resource}.linkTitle`]}</a>
+    <div className={styles.resourceLink}>{copy[`${resource}.linkTitle`]}</div>
   </div>
 )
