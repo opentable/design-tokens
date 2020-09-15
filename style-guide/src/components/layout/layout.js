214216b437ecfca3554484a8791d4341994f0cec
nsingh <nsingh@opentable.com>
2020-09-15 10:27:13 +0530

    fix(style-guide): fix window is not defined error

--- style-guide/src/components/layout/layout.js
+++ style-guide/src/components/layout/layout.js
@@ -4,31 +4,40 @@ import { Link } from 'gatsby';
 import styles from '../../styles/index.module.scss';
 import logo from '../../assets/logo.svg';
 
-export default ({ children, nav = null, className = '' }) => (
-  <div>
-    <div className={styles.header}>
-      <Link to="/" style={{ marginRight: '60px' }}>
-        <img alt="Opentable logo" src={logo} className={styles['logo']} />
-        <h1>Design Bar</h1>
-      </Link>
-      <ul>
-        <li>
-          <Link to="/" className={styles.headerLink} activeClassName={styles.headerLinkActive} partiallyActive={window.location.pathname.includes('/otkit/')}>
-            Design tokens
-          </Link>
-        </li>
-        <li>
-          <Link to="/handover-process" className={styles.headerLink} activeClassName={styles.headerLinkActive}>
-            Process guidelines
-          </Link>
-        </li>
-      </ul>
-    </div>
+export default ({ children, nav = null, className = '' }) => {
+
+  function getPartiallyActive() {
+    if (typeof window !== 'undefined') {
+      return window.location.pathname.includes('/otkit/');
+    }
+  }
+
+  return (
     <div>
-      {nav ? <div className={styles.nav}>{nav}</div> : null}
-      <div className={styles.mainBody}>
-        <div className={`${styles.main} ${className}`}>{children}</div>
+      <div className={styles.header}>
+        <Link to="/" style={{ marginRight: '60px' }}>
+          <img alt="Opentable logo" src={logo} className={styles['logo']} />
+          <h1>Design Bar</h1>
+        </Link>
+        <ul>
+          <li>
+            <Link to="/" className={styles.headerLink} activeClassName={styles.headerLinkActive} partiallyActive={getPartiallyActive()}>
+              Design tokens
+            </Link>
+          </li>
+          <li>
+            <Link to="/handover-process" className={styles.headerLink} activeClassName={styles.headerLinkActive}>
+              Process guidelines
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        {nav ? <div className={styles.nav}>{nav}</div> : null}
+        <div className={styles.mainBody}>
+          <div className={`${styles.main} ${className}`}>{children}</div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  )
+};
